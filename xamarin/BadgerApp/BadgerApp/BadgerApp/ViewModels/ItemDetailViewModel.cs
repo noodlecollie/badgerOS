f8773d6fdbe084e7b89b9e6e96475e21b5cd32c5
Jonathan Poncelet <jonathan.poncelet@talk21.com>
2020-04-18 13:34:26 +0100

    Added template mobile app

---
+++ xamarin/BadgerApp/BadgerApp/BadgerApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BadgerApp.Models;
+
+namespace BadgerApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
